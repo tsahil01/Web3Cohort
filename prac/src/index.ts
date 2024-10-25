8122e0223eaa7996a78dd15dcd351c72db4ad533
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-10-25 12:19:59 +0530

    practice - public - private - msg sign on solana

---
+++ prac/src/index.ts
@@ -0,0 +1,22 @@
+import { Keypair } from "@solana/web3.js";
+import nacl from "tweetnacl";
+
+// Generate a new random keypair
+const keyPair = Keypair.generate();
+
+const publicKey = keyPair.publicKey;
+const secretKey = keyPair.secretKey;
+
+console.log("Public Key:", publicKey.toString());
+console.log("Secret Key:", secretKey);
+
+const message = new TextEncoder().encode("hello world");
+
+console.log("Message:", message);
+
+const signature = nacl.sign.detached(message, secretKey);
+console.log("Signature:", signature);
+
+const result = nacl.sign.detached.verify(message, signature, publicKey.toBytes());
+
+console.log("Result:", result);
